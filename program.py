b7e2604444b9f2d42969e091b1da55cca149abd8
Hiranmay Mandal <hiranmay1000@gmail.com>
2023-12-17 23:32:23 +0530

    FIX: thisenv

--- program.py
+++ program.py
@@ -1,13 +1,26 @@
-import SpeechRecognition as sr
+print("Code entry...")
+import speech_recognition as sr
 import pyttsx3 as tt
 
-listener = sr.Recognizer();
+listener = sr.Recognizer()
+
 try:
     with sr.Microphone() as source:
-        print("Listening...")
-        voice = listener.listen(source);
-        command = listener.recognizer_google(voice)
-        print(command);
+        print("\n\nListening...")
+        ret, voice = listener.listen(source);
+        if ret:
+            print("Please wait while proccessing your request...");
+        command = listener.recognize_google(voice)
+        print(command)
+        
 
 except:
     pass
+
+print("\n\n\nCode end...")
+
+
+# !DEPENDENCIES
+# sudo apt-get install portaudio19-dev
+# pip install pyaudio
+
